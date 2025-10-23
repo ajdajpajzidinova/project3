f24899d7de3c0ad7908efd96c4cf8312389aed18
ajdajpajzidinova <ajdajpajzidinova.gmail.com>
2025-10-23 10:24:03 +0600

    added.gitignore

--- main3.py
+++ main3.py
@@ -1,1 +1,13 @@
 print('hello world')
+
+
+def check_word(name):
+    if name == name[::-1]:
+        print('Палиндром')
+    else:
+        print('Палиндром эмес')
+
+check_word('ata')
+check_word('car')
+check_word('python')
+check_word('madam')
---
+++ secrets.py
@@ -0,0 +1,2 @@
+passowrd='yellow'
+secret_key='blue'
